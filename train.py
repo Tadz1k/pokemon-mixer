1e8a88cbbec2f35eb0b411b0b4a7b8e529da2912
Tadz1k <50612974+Tadz1k@users.noreply.github.com>
2021-12-20 20:38:29 +0100

    Ver 1.1 - automatyczna konwersja RGBA do RGB

--- train.py
+++ train.py
@@ -1,52 +1,83 @@
 from fastbook import *
 from fastai.vision.widgets import *
-
-path = Path('./dataset')
-
-classes = ()
-
-#dynamic import classes
-classes = tuple(os.listdir('dataset'))
-
-#check
-print(classes)
-
-#prepare data - conver svg to png files
+from fastai.vision import *
+from fastai.metrics import *
+#images
 import os
 from svglib.svglib import svg2rlg
 from reportlab.graphics import renderPM
+from PIL import Image
 
-for dir in classes:
-	for file in os.listdir(f'dataset/{dir}'):
-		if '.svg' in file:
-			svg_path = (f'dataset/{dir}/{file}')
-			drawing = svg2rlg(svg_path)
-			png_file = file.replace('.svg', '.png')
-			file_path = (f'dataset/{dir}/{png_file}')
-			#save file to png
-			renderPM.drawToFile(drawing, file_path, fmt='PNG')
-			#delete svg old
-			os.remove(svg_path)
+def main():
+	path = Path('./dataset')
 
-#load once more classes
-classes = tuple(os.listdir('dataset'))
+	classes = ()
 
-data = DataBlock(
-    blocks=(ImageBlock, CategoryBlock),
-    get_items=get_image_files,
-    splitter=RandomSplitter(valid_pct=0.2,seed=42),
-    get_y=parent_label,
-    item_tfms=Resize(128)
-)
+	#dynamic import classes
+	classes = tuple(os.listdir('dataset'))
 
-data = data.new(
-    item_tfms=RandomResizedCrop(224, min_scale=0.5),
-    batch_tfms=aug_transforms(mult=0.0, do_flip=False, flip_vert=False, max_rotate=0.0, min_zoom=0.0, max_zoom=0.0, max_lighting=0.0, max_warp=0.0, p_affine=0.0, p_lighting=0.0, xtra_tfms=None, size=None, mode='bilinear', pad_mode='border', align_corners=True, batch=False, min_scale=1.0))
+	#check
+	print(classes)
 
-dls = data.dataloaders(path, bs = 32)
+	#prepare data - conver svg to png files
 
-learn = cnn_learner(dls, alexnet, metrics=error_rate)
+	print('==================================================')
+	print('Konwertowanie zdjęć....')
+	print('==================================================')
+	for dir in classes:
+		for file in os.listdir(f'dataset/{dir}'):
+			file_path = (f'dataset/{dir}/{file}')
+			if '.svg' in file:
+				jpg_file = file.replace('.svg', '.jpg')
+				jpg_path = (f'dataset/{dir}/{jpg_file}')
+				svg_path = (f'dataset/{dir}/{file}')
+				drawing = svg2rlg(svg_path)                         #load svg to memory
+				print(f'{dir}/{file} ===> {dir}/{jpg_file}')
+				os.remove(svg_path)                                 #delete svg file
+				renderPM.drawToFile(drawing, jpg_path, fmt='JPG')   #convert svg to jpg and save
+			if '.png' in file:
+				jpg_file = file.replace('.png', '.jpg')
+				jpg_path = (f'dataset/{dir}/{jpg_file}')
 
-learn.fit_one_cycle(1) #jedna epoka
-learn.save('a3.1.1.1')
-learn.export(fname="model.pkl")
+				png_image = Image.open(file_path).convert('RGBA')               #load png file with alpha
+				background = Image.new("RGBA", png_image.size, (255,255,255))   #create white background
+				alpha_composite = Image.alpha_composite(background, png_image)  #replace alpha with white color
+				alpha_composite_3 = alpha_composite.convert('RGB')              #convert to RGB
+				print(f'{dir}/{file} ===> {dir}/{jpg_file}')
+				os.remove(file_path)                                            #delete png file
+				alpha_composite_3.save(jpg_path, 'JPEG', quality=100)           #save jpg file
+
+	#load once more classes
+	classes = tuple(os.listdir('dataset'))
+	print('==================================================')
+	print('KONIEC KONWERTOWANIA ZDJĘĆ...')
+	print('==================================================')
+
+	data = DataBlock(
+    	blocks=(ImageBlock, CategoryBlock),
+    	get_items=get_image_files,
+    	splitter=RandomSplitter(valid_pct=0.2,seed=42),
+    	get_y=parent_label,
+    	item_tfms=Resize(128)
+	)
+
+	data = data.new(
+    	item_tfms=RandomResizedCrop(224, min_scale=0.5),
+    	batch_tfms=aug_transforms(mult=0.0, do_flip=False, flip_vert=False, max_rotate=0.0, min_zoom=0.0, max_zoom=0.0, max_lighting=0.0, max_warp=0.0, p_affine=0.0, p_lighting=0.0, xtra_tfms=None, size=None, mode='bilinear', pad_mode='border', align_corners=True, batch=False, min_scale=1.0))
+
+	dls = data.dataloaders(path, bs = 32)
+
+	learn = cnn_learner(dls, models.resnet101, metrics = accuracy)
+
+	#earn.fit_one_cycle(5)
+	#learn.save('a3.1.1.1')
+	#learn.export(fname="model.pkl")
+
+	for i in range(10): #20 epok
+		print(f'iteracja : {i}')
+		learn.fit_one_cycle(1)
+		learn.save(f'a3.1.1.{i}')
+		learn.export(fname=f'learned_pickles/model{i}.pkl')
+
+if __name__ == '__main__':
+	main() #added for freeze support
