3afbc76b551966154fe898a5aeb95da2fd4588c6
s18345 <s18345@pjwstk.edu.pl>
2022-01-11 00:54:16 +0100

    Add card animation

--- static/js/home.js
+++ static/js/home.js
@@ -1,24 +1,22 @@
 $(document).ready(function () {
-
-  // // Upload Preview
-  // function readURL(input) {
-  //     if (input.files && input.files[0]) {
-  //         let reader = new FileReader();
-  //         reader.onload = function (e) {
-  //             $('#imagePreview').css('background-image', 'url(' + e.target.result + ')');
-  //             $('#imagePreview').hide();
-  //             $('#imagePreview').fadeIn(650);
-  //         }
-  //         reader.readAsDataURL(input.files[0]);
-  //     }
-  // }
-  // $("#imageUpload").change(function () {
-  //     $('.image-section').show();
-  //     $('#btn-predict').show();
-  //     $('#result').text('');
-  //     $('#result').hide();
-  //     readURL(this);
-  // });
+  const typeColor = {
+    bug: "#26de81",
+    dragon: "#ffeaa7",
+    electric: "#fed330",
+    fairy: "#FF0069",
+    fighting: "#30336b",
+    fire: "#f0932b",
+    flying: "#81ecec",
+    grass: "#00b894",
+    ground: "#EFB549",
+    ghost: "#a55eea",
+    ice: "#74b9ff",
+    normal: "#95afc0",
+    poison: "#6c5ce7",
+    psychic: "#a29bfe",
+    rock: "#2d3436",
+    water: "#0190FF",
+  };
 
   // Predict
   $('#btn-predict').click(function () {
@@ -32,13 +30,63 @@ $(document).ready(function () {
       processData: false,
       async: true,
       success: function (data) {
-        // Get and display the result
-        // $('.loader').hide();
-        // $('#result').fadeIn(600);
-        // $('#result').text(' Result:  ' + data);
-        console.log('Success!');
-      },
+        $('#result').fadeIn(600);
+        const parent1card = document.getElementById("pokemon_parent1_card");
+        const parent1CardThemeColor = typeColor[data.pokemon_parent1.type.toLowerCase()];
+        $('#pokemon_parent1_hp').text('HP ' + data.pokemon_parent1.hp);
+        $("#pokemon_parent1_img").attr("src",data.pokemon_parent1.image);
+        $('#pokemon_parent1_id').text(`#${data.pokemon_parent1.id}`);
+        $('#pokemon_parent1_name').text(data.pokemon_parent1.name);
+        $("#pokemon_parent1_attack").text(data.pokemon_parent1.attack);
+        $("#pokemon_parent1_defense").text(data.pokemon_parent1.defense);
+        $("#pokemon_parent1_speed").text(data.pokemon_parent1.speed);
+        $("#pokemon_parent1_speedattack").text(data.pokemon_parent1.speedattack);
+        $("#pokemon_parent1_speeddefense").text(data.pokemon_parent1.speeddefense);
+        $("#pokemon_parent1_total").text(data.pokemon_parent1.total);
+        $('#pokemon_parent1_type span').text(data.pokemon_parent1.type);
+        $('#pokemon_parent1_id').css('color', parent1CardThemeColor);
+        $('#pokemon_parent1_type span').css('background-color', parent1CardThemeColor);
+        parent1card.style.background = `radial-gradient(circle at 50% 0%, ${parent1CardThemeColor} 36%, #ffffff 36%)`;
+
+        const parent2card = document.getElementById("pokemon_parent2_card");
+        const parent2CardThemeColor = typeColor[data.pokemon_parent2.type.toLowerCase()];
+        $('#pokemon_parent2_hp').text('HP ' + data.pokemon_parent2.hp);
+        $("#pokemon_parent2_img").attr("src",data.pokemon_parent2.image);
+        $('#pokemon_parent2_id').text(`#${data.pokemon_parent2.id}`);
+        $('#pokemon_parent2_name').text(data.pokemon_parent2.name);
+        $("#pokemon_parent2_attack").text(data.pokemon_parent2.attack);
+        $("#pokemon_parent2_defense").text(data.pokemon_parent2.defense);
+        $("#pokemon_parent2_speed").text(data.pokemon_parent2.speed);
+        $("#pokemon_parent2_speedattack").text(data.pokemon_parent2.speedattack);
+        $("#pokemon_parent2_speeddefense").text(data.pokemon_parent2.speeddefense);
+        $("#pokemon_parent2_total").text(data.pokemon_parent2.total);
+        $('#pokemon_parent2_type span').text(data.pokemon_parent2.type);
+        $('#pokemon_parent2_id').css('color', parent2CardThemeColor);
+        $('#pokemon_parent2_type span').css('background-color', parent2CardThemeColor);
+        parent2card.style.background = `radial-gradient(circle at 50% 0%, ${parent2CardThemeColor} 36%, #ffffff 36%)`;
+
+
+        const hybridcard = document.getElementById("pokemon_hybrid_card");
+        const hybridCardThemeColor = typeColor[data.pokemon_hybrid.type.toLowerCase()];
+        $('#pokemon_hybrid_hp').text('HP ' + data.pokemon_hybrid.hp);
+        $("#pokemon_hybrid_img").attr("src",data.pokemon_hybrid.image);
+        $('#pokemon_hybrid_id').text(`#${data.pokemon_hybrid.id}`);
+        $('#pokemon_hybrid_name').text(data.pokemon_hybrid.name);
+        $("#pokemon_hybrid_attack").text(data.pokemon_hybrid.attack);
+        $("#pokemon_hybrid_defense").text(data.pokemon_hybrid.defense);
+        $("#pokemon_hybrid_speed").text(data.pokemon_hybrid.speed);
+        $("#pokemon_hybrid_speedattack").text(data.pokemon_hybrid.speedattack);
+        $("#pokemon_hybrid_speeddefense").text(data.pokemon_hybrid.speeddefense);
+        $("#pokemon_hybrid_total").text(data.pokemon_hybrid.total);
+        $('#pokemon_hybrid_type span').text(data.pokemon_hybrid.type);
+        $('#pokemon_hybrid_id').css('color', hybridCardThemeColor);
+        $('#pokemon_hybrid_type span').css('background-color', hybridCardThemeColor);
+        hybridcard.style.background = `radial-gradient(circle at 50% 0%, ${hybridCardThemeColor} 36%, #ffffff 36%)`;
+      }
     });
+  });
+  $('#btn-back').click(function () {
+        $('#result').fadeOut(1000);
   });
   const imagesTypes = [
     "jpeg",
